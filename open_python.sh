1f573791b9d2a6a15c88b0a32e3a6f6c855e37f8
adefgreen98 <adefgreen98@gmail.com>
2022-09-24 11:56:50 +0200

    Removed non-relevant scripts

--- open_python.sh
+++
@@ -1,2 +0,0 @@
-devcnt=$1
-CUDA_VISIBLE_DEVICES="$devcnt" /home/federico.pedeni/internship_venv/bin/python3
--- run_script.sh
+++
@@ -1,5 +0,0 @@
-devcnt=$1
-scrname=$2
-shift
-shift
-CUDA_VISIBLE_DEVICES="$devcnt" /home/federico.pedeni/internship_venv/bin/python3 "$scrname" $@
